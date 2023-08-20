42a28f80f0176cea6aea8760964c24611a8b1bcd
typeonwood <typeonwood@gmail.com>
2023-08-20 10:52:01 -0700

    create new game dialog

--- script.js
+++ script.js
@@ -41,45 +41,39 @@ const displayBoard = (function() {
 const Game = (function() {
     const squares = document.querySelectorAll('.square')
     const announcement = document.querySelector('.announcement')
-    const name1 = prompt('Enter name for Player 1')
-    const name2 = prompt('Enter name for Player 2')
-    const player1 = Player(name1)
-    const player2 = Player(name2, false)
+    let name1 = ''
+    let name2 = ''
+    const modal = document.querySelector('dialog');
+    let player1 = {}
+    let player2 = {}
     let count = 0;
+    const selectWinner = (num1, num2, num3) => {
+        const winner1 = document.querySelector(`#square${num1}`)
+        const winner2 = document.querySelector(`#square${num2}`)
+        const winner3 = document.querySelector(`#square${num3}`);
+        winner1.classList.add('winner')
+        winner2.classList.add('winner')
+        winner3.classList.add('winner');
+        return true
+    }
     const checker = () => {
         const board = gameBoard.board;
         for (i=1; i<8; i+=3) {
             if (board[i] === board[i-1] &&
                 board[i-1] === board[i+1] &&
-                board[i+1] !== '') {
-                const winner = document.querySelector(`#square${i}`);
-                winner.classList.add('winner')
-                return true
-            }
+                board[i+1] !== '') return selectWinner(i-1, i, i+1)
         }
         for (i=3; i<6; i++) {
             if (board[i] === board[i-3] &&
                 board[i-3] === board[i+3] &&
-                board[i+3] !== '') {
-                const winner = document.querySelector(`#square${i}`);
-                winner.classList.add('winner')
-                return true
-            }
+                board[i+3] !== '') return selectWinner(i-3, i, i+3)
         }
         if (board[0] === board[4] &&
             board[4] === board[8] &&
-            board[8] !== '') {
-            const winner = document.querySelector('#square4');
-            winner.classList.add('winner')
-            return true
-        }
+            board[8] !== '') return selectWinner(0, 4, 8)
         if (board[2] === board[4] &&
             board[4] === board[6] &&
-            board[6] !== '') {
-            const winner = document.querySelector('#square4');
-            winner.classList.add('winner')
-            return true
-        }
+            board[6] !== '') return selectWinner(2, 4, 6)
         else return false
     }
     const player1Turn = (e) => {
@@ -111,7 +105,7 @@ const Game = (function() {
         }
     }
     const endGame = () => {
-        const winner = document.querySelector('.winner')
+        const winner = document.querySelector('.winner');
         if (winner.textContent === 'X') {
             announcement.textContent = `Congratulations, ${player1.name}. You win!`
             squares.forEach((square) => {
@@ -126,16 +120,31 @@ const Game = (function() {
                 square.removeEventListener('click', player1Turn)
             })
         }
-        winner.classList.remove()
     }
     const play = () => {
+        if (document.querySelector('.winner')) {
+            const winners = document.querySelectorAll('.winner')
+            winners.forEach((winner) => {
+                winner.classList.remove('winner')
+            })
+        }
         gameBoard.board = ['','','','','','','','','']
         squares.forEach((square) => {
             square.textContent = ''
         })
         announcement.textContent = ''
-        count = 0;
-        turns()
+        count = 0
+        modal.showModal();
+        const form = document.querySelector('form');
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            name1 = document.querySelector('#p1-name')
+            name2 = document.querySelector('#p2-name');
+            player1 = Player(name1.value)
+            player2 = Player(name2.value, false)
+            modal.close();
+            turns()
+        })
     }
     const newGame = document.querySelector('.new-game');
     newGame.addEventListener('click', play)
