ebaedcd1fae03fff210c62a9500d313019e531b5
typeonwood <typeonwood@gmail.com>
2023-08-19 17:10:10 -0700

    create Game object with functions

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const displayBoard = (function() {
     }
     const display = () => {
         gameBoard.board.forEach((square, index) => {
-            const displaySquare = document.querySelector(`#${index}`)
+            const displaySquare = document.querySelector(`#square${index}`)
             if (square === true) {
                 displaySquare.textContent = 'X'
             }
@@ -35,4 +35,42 @@ const displayBoard = (function() {
     return {create, display}
 })()
 
+const Game = (function() {
+    const checker = () => {
+        const board = gameBoard.board
+        for (i=1; i<8; i+=3) {
+            if (board[i] === board[i-1] === board[i+1]) return true
+        }
+        for (i=3; i<6; i++) {
+            if (board[i] === board[i-3] === board[i+3]) return true
+        }
+        if (board[0] === board[4] === board[8]) return true
+        if (board[2] === board[4] === board[6]) return true
+        else return false
+    }
+    const play = () => {
+        const name1 = prompt('Enter name for Player 1')
+        const name2 = prompt('Enter name for Player 2')
+        const player1 = Player(name1)
+        const player2 = Player(name2, false)
+        const squares = document.querySelectorAll('.square')
+        let count = 0;
+        while (!checker()) {
+            if (count % 2 === 0) {
+                squares.forEach((square) => {
+                    square.addEventListener('click', player1.move)
+                })
+            }
+            else {
+                squares.forEach((square) => {
+                    square.addEventListener('click', player2.move)
+                })
+            }
+            displayBoard.display()
+        }
+    }
+    return {play}
+})()
+
 displayBoard.create()
+displayBoard.display()
